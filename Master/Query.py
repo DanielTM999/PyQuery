2fbbabbab322b81d70959318687431fbac00b744
Daniel Teixeira Melo <108521502+DanielTM999@users.noreply.github.com>
2023-03-02 19:02:31 -0300

    On branch master  Your branch is up to date with 'origin/master'. 	modified:   Master/Query.py

--- Master/Query.py
+++ Master/Query.py
@@ -50,7 +50,7 @@ class Masterclass:
         finally:
             act[0].close()
             act[1].close()
-            
+
     def dropDB(self, database):
         if(self.showemsg):
             print("*"*10+"EXCLUINDO DB"+"*"*10)
@@ -139,7 +139,7 @@ class Masterclass:
             return conexao, cursor
         except:
             print("Erro de coneção com o banco de dados")
-    
+
     def useDB(self, database):
         self.database = database
 
@@ -154,7 +154,7 @@ class Masterclass:
             err = abs(len(type) - len(colluns))
             if(len(colluns) < len(type)):
                 for i in range(0, err):
-                    colluns += ["args"] 
+                    colluns += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}\033[0;0m"\033[0m')
             elif(len(colluns) + len(type) == 0):
                 colluns += ["args, ..."]
@@ -162,10 +162,10 @@ class Masterclass:
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}/{type}\033[0;0m"\033[0m')
             else:
                 for i in range(0, err):
-                    type += ["args"] 
+                    type += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{type}\033[0;0m"\033[0m')
             exit()
-        
+
         for i in range(0, len(colluns)):
             if(len(colluns[i].strip()) == 0):
                 print('\033[31m->Erro: elemento em branco \033[0;0m'+ f'\033[1m"\033[32m{colluns}Collun\033[0;0m"\033[0m')
@@ -194,7 +194,7 @@ class Masterclass:
                 pste+= f"{colluns[i]} {type[i]}({tamanho[i]}) NOT NULL,"
             elif(type[i] == "int"):
                 pste+= f"{colluns[i]} {type[i]} NOT NULL,"
-        
+
         try:
             if(self.showemsg):
                 print("\033[32mErros: 0\033[0;0m")
@@ -202,7 +202,7 @@ class Masterclass:
                 print("\033[32mPreparando query...\033[0;0m")
                 sleep(1)
             sql = f"CREATE TABLE IF NOT EXISTS {tableName}(id{tableName} INT NOT NULL AUTO_INCREMENT,{pste} PRIMARY KEY (`id{tableName}`))"
-            if(self.showemsg): 
+            if(self.showemsg):
                 print("\033[32mcriando conexão com o banco...\033[0;0m")
                 sleep(1)
             act = self.connexao()
@@ -223,7 +223,7 @@ class Masterclass:
         finally:
             act[0].close()
             act[1].close()
-    
+
     def insertDB(self, tableName, colluns = list, values = list):
         if(self.showemsg):
             print("*"*10+"INSERINDO VALORES"+"*"*10)
@@ -237,7 +237,7 @@ class Masterclass:
             err = abs(len(values) - len(colluns))
             if(len(colluns) < len(values)):
                 for i in range(0, err):
-                    colluns += ["args"] 
+                    colluns += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}colluns\033[0;0m"\033[0m')
             elif(len(colluns) + len(values) == 0):
                 colluns += ["args, ..."]
@@ -245,10 +245,10 @@ class Masterclass:
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}/{values}colluns/values\033[0;0m"\033[0m')
             else:
                 for i in range(0, err):
-                    values += ["args"] 
+                    values += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{values}values\033[0;0m"\033[0m')
             exit()
-        
+
         for i in range(0, len(colluns)):
             values[i] = str(values[i])
             if(len(colluns[i].strip()) == 0):
@@ -263,7 +263,7 @@ class Masterclass:
                 if(values[i] == "-1"):
                     exit()
                 print('\033[31m->Erro: elemento em branco \033[0;0m'+ f'\033[1m"\033[32m{values}Value\033[0;0m"\033[0m')
-        
+
         for i in range(0, len(values)):
             if(values[i].isnumeric()):
                 pste_itens += f"{colluns[i]},"
@@ -305,7 +305,7 @@ class Masterclass:
         finally:
             act[0].close()
             act[1].close()
-        
+
     def getAllDB(self, TableName):
         banco = self.database
         if(len(TableName.strip()) == 0):
@@ -314,20 +314,25 @@ class Masterclass:
             if(TableName == "-1"):
                 exit()
         try:
-            print("\033[32mErros: 0\033[0;0m")
-            sleep(1)
-            print("\033[32mPreparando query...\033[0;0m")
+            if(self.showemsg):
+                print("\033[32mErros: 0\033[0;0m")
+                sleep(1)
+                print("\033[32mPreparando query...\033[0;0m")
             sql = f"SELECT * FROM {banco}.{TableName}"
-            sleep(1)
-            print("\033[32mcriando conexão com o banco...\033[0;0m")
+            if(self.showemsg):
+                sleep(1)
+                print("\033[32mcriando conexão com o banco...\033[0;0m")
             act = self.connexao()
-            sleep(1)
-            print("\033[32mvalidando...\033[0;0m")
+            if(self.showemsg):
+                sleep(1)
+                print("\033[32mvalidando...\033[0;0m")
             act[1].execute(sql)
-            print(f"\033[32mRetornando valores em array...\033[0;0m")
+            if(self.showemsg):
+                print(f"\033[32mRetornando valores em array...\033[0;0m")
             linhas  = act[1].fetchall()
-            sleep(1)
-            print(f"\033[32mSucesso\033[0;0m")
+            if(self.showemsg):
+                sleep(1)
+                print(f"\033[32mSucesso\033[0;0m")
             return linhas
         except Exception as e:
             print("fatal error, não foi possivel acessa o mysql")
@@ -335,7 +340,7 @@ class Masterclass:
         finally:
             act[0].close()
             act[1].close()
-        
+
     def getUniqIdDB(self, TableName, colluns = list, itens = list, elementos = ""):
         pste_elemento = ""
         pste_itens = ""
@@ -344,14 +349,14 @@ class Masterclass:
             pste_elemento = "*"
         else:
             for i in elementos:
-                pste_elemento += f"{i},"   
+                pste_elemento += f"{i},"
             pste_elemento = pste_elemento[0:(len(pste_elemento)-1)]
 
         if(len(colluns) != len(itens) or (len(colluns) + len(itens) == 0)):
             err = abs(len(itens) - len(colluns))
             if(len(colluns) < len(itens)):
                 for i in range(0, err):
-                    colluns += ["args"] 
+                    colluns += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}\033[0;0m"\033[0m')
             elif(len(colluns) + len(itens) == 0):
                 colluns += ["args, ..."]
@@ -359,9 +364,9 @@ class Masterclass:
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}/{itens}\033[0;0m"\033[0m')
             else:
                 for i in range(0, err):
-                    itens += ["args"] 
+                    itens += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{itens}\033[0;0m"\033[0m')
-            exit() 
+            exit()
         for i in range(0, len(colluns)):
             if(len(colluns[i].strip()) == 0):
                 print('\033[31m->Erro: elemento em branco \033[0;0m'+ f'\033[1m"\033[32m{colluns}Collun\033[0;0m"\033[0m')
@@ -378,22 +383,27 @@ class Masterclass:
         for i in range(0, len(itens)):
             pste_itens += f"{colluns[i]}='{itens[i]}' AND "
         pste_itens = pste_itens[0:(len(pste_itens)-5)]
-            
-        try:    
-            print("\033[32mErros: 0\033[0;0m")
-            sleep(1)
-            print("\033[32mPreparando query...\033[0;0m")
+
+        try:
+            if(self.showemsg):
+                print("\033[32mErros: 0\033[0;0m")
+                sleep(1)
+                print("\033[32mPreparando query...\033[0;0m")
             sql = f"SELECT {pste_elemento} FROM {banco}.{TableName} WHERE {pste_itens}"
-            sleep(1)
-            print("\033[32mcriando conexão com o banco...\033[0;0m")
+            if(self.showemsg):
+                sleep(1)
+                print("\033[32mcriando conexão com o banco...\033[0;0m")
             act = self.connexao()
-            sleep(1)
-            print("\033[32mvalidando...\033[0;0m")
+            if(self.showemsg):
+                sleep(1)
+                print("\033[32mvalidando...\033[0;0m")
             act[1].execute(sql)
-            print(f"\033[32mRetornando valores em array...\033[0;0m")
+            if(self.showemsg):
+                print(f"\033[32mRetornando valores em array...\033[0;0m")
             linhas  = act[1].fetchone()
-            sleep(1)
-            print(f"\033[32mSucesso\033[0;0m")
+            if(self.showemsg):
+                sleep(1)
+                print(f"\033[32mSucesso\033[0;0m")
             return linhas
         except Exception as e:
             print("fatal error, não foi possivel acessa o mysql")
@@ -401,7 +411,7 @@ class Masterclass:
         finally:
             act[0].close()
             act[1].close()
-    
+
     def AlterElement(self, NameTable, colluns = list, newValue = list, Condition = list):
         banco = self.database
 
@@ -409,7 +419,7 @@ class Masterclass:
             err = abs(len(newValue) - len(colluns))
             if(len(colluns) < len(newValue)):
                 for i in range(0, err):
-                    colluns += ["args"] 
+                    colluns += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}\033[0;0m"\033[0m')
             elif(len(colluns) + len(newValue) == 0):
                 colluns += ["args, ..."]
@@ -417,7 +427,7 @@ class Masterclass:
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}/{type}\033[0;0m"\033[0m')
             else:
                 for i in range(0, err):
-                    newValue += ["args"] 
+                    newValue += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{newValue}\033[0;0m"\033[0m')
             exit()
 
@@ -425,7 +435,7 @@ class Masterclass:
         for i in range(len(colluns)):
             pste += f"{colluns[i]}='{newValue[i]}',"
         pste = pste[0:(len(pste)-1)]
-        
+
         try:
             if(self.showemsg):
                 print("\033[32mErros: 0\033[0;0m")
@@ -433,7 +443,7 @@ class Masterclass:
                 print("\033[32mPreparando query...\033[0;0m")
                 sleep(1)
             sql = f"UPDATE `{banco}`.`{NameTable}` SET {pste} WHERE {Condition[0]}='{Condition[1]}'"
-            if(self.showemsg): 
+            if(self.showemsg):
                 print("\033[32mcriando conexão com o banco...\033[0;0m")
                 sleep(1)
             act = self.connexao()
@@ -494,7 +504,7 @@ class Masterclass:
                 print("\033[32mPreparando query...\033[0;0m")
                 sleep(1)
             sql = f"ALTER TABLE `{banco}`.`{NameTable}` {pste}"
-            if(self.showemsg): 
+            if(self.showemsg):
                 print("\033[32mcriando conexão com o banco...\033[0;0m")
                 sleep(1)
             act = self.connexao()
@@ -524,14 +534,14 @@ class Masterclass:
             pste_elemento = "*"
         else:
             for i in elementos:
-                pste_elemento += f"{i},"   
+                pste_elemento += f"{i},"
             pste_elemento = pste_elemento[0:(len(pste_elemento)-1)]
 
         if(len(colluns) != len(itens) or (len(colluns) + len(itens) == 0)):
             err = abs(len(itens) - len(colluns))
             if(len(colluns) < len(itens)):
                 for i in range(0, err):
-                    colluns += ["args"] 
+                    colluns += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}\033[0;0m"\033[0m')
             elif(len(colluns) + len(itens) == 0):
                 colluns += ["args, ..."]
@@ -539,9 +549,9 @@ class Masterclass:
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{colluns}/{itens}\033[0;0m"\033[0m')
             else:
                 for i in range(0, err):
-                    itens += ["args"] 
+                    itens += ["args"]
                 print('\033[31m->Erro: Faltando elemento \033[0;0m'+ f'\033[1m"\033[32m{itens}\033[0;0m"\033[0m')
-            exit() 
+            exit()
         for i in range(0, len(colluns)):
             if(len(colluns[i].strip()) == 0):
                 print('\033[31m->Erro: elemento em branco \033[0;0m'+ f'\033[1m"\033[32m{colluns}Collun\033[0;0m"\033[0m')
@@ -558,23 +568,28 @@ class Masterclass:
         for i in range(0, len(itens)):
             pste_itens += f"{colluns[i]}='{itens[i]}' AND "
         pste_itens = pste_itens[0:(len(pste_itens)-5)]
-            
-        try:    
-            print("\033[32mErros: 0\033[0;0m")
-            sleep(1)
-            print("\033[32mPreparando query...\033[0;0m")
+
+        try:
+            if(self.showemsg):
+                print("\033[32mErros: 0\033[0;0m")
+                sleep(1)
+                print("\033[32mPreparando query...\033[0;0m")
             sql = f"SELECT {pste_elemento} FROM {banco}.{TableName} WHERE {pste_itens}"
-            sleep(1)
-            print("\033[32mcriando conexão com o banco...\033[0;0m")
+            if(self.showemsg):
+                sleep(1)
+                print("\033[32mcriando conexão com o banco...\033[0;0m")
             act = self.connexao()
-            sleep(1)
-            print("\033[32mvalidando...\033[0;0m")
+            if(self.showemsg):
+                sleep(1)
+                print("\033[32mvalidando...\033[0;0m")
             act[1].execute(sql)
-            print(f"\033[32mRetornando valores em array...\033[0;0m")
+            if(self.showemsg):
+                print(f"\033[32mRetornando valores em array...\033[0;0m")
             act[1].fetchone()
             linhas  = act[1].rowcount
-            sleep(1)
-            print(f"\033[32mSucesso\033[0;0m")
+            if(self.showemsg):
+                sleep(1)
+                print(f"\033[32mSucesso\033[0;0m")
             return linhas
         except Exception as e:
             print("fatal error, não foi possivel acessa o mysql")
@@ -589,7 +604,7 @@ class DotEnv:
         dic = []
         for i in env:
             dic.append(os.getenv(i))
-        
+
         return dic
 
 class MasterclassAuth(Masterclass):
